783953ee18ca05fe25501e33f00b817a5ac718d5
xiangsi <xiangsi@xiangsi.com>
2024-10-14 21:08:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,200 @@
+<template>
+    <div class="container">
+        <br />
+        <div class="box">
+            <div class="profile-bar">
+                <div class="header-wrapper">
+                    <div
+                        class="icon-wrapper"
+                        :style="{ background: `conic-gradient(rgb(97, 152, 253), rgb(97, 152, 253) ${ratio}%, rgb(219, 219, 219) ${ratio}%) ` }"
+                    >
+                        <img class="header" src="./assets/icon.jpg" />
+                    </div>
+                    <div class="score">331</div>
+                </div>
+                <div class="summoner-info">
+                    <div class="tag is-light is-size-7"><b>2024赛季</b></div>
+                    <div class="is-size-4">
+                        <b>哈哈哈</b>
+                        <span>&nbsp;</span>
+                        <span class="has-text-grey">#12345</span>
+                    </div>
+                    <div>
+                        <span>当前生涯状态</span>
+                        <span>&nbsp;</span>
+                        <span
+                            class="tag has-text-white"
+                            :class="{
+                                'is-warning': isPublic,
+                                'is-danger': !isPublic
+                            }"
+                        >
+                            公开
+                        </span>
+                    </div>
+                    <div class="tag is-size-6">
+                        <font-awesome-icon icon="fas fa-angry" />
+                        <font-awesome-icon icon="user-secret" />
+                        <span>&nbsp;</span>
+                        联盟二区
+                    </div>
+                    <div class="has-text-grey is-size-7">上次在线时间2个月前</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="box sporting-wrapper">
+            <div class="sporting-list">
+                <div
+                    class="sporting-item"
+                    v-for="(item, idx) in sportingList"
+                    :class="{ active: item.active }"
+                    @click="handleClickSportingItem(item)"
+                    :style="{ background: `conic-gradient(rgb(97, 152, 253), rgb(97, 152, 253) 70%, rgb(219, 219, 219) 70%%) ` }"
+                >
+                  <img class="list-header" src="./assets/icon.jpg" height="50" width="50" />
+                    {{ item.name }}
+                </div>
+            </div>
+            <div class="sporting-detail"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue"
+
+let ratio = ref(60)
+let isPublic = ref(true)
+
+let sportingList = reactive([
+    {
+        active: true,
+        name: "Heloworld"
+    },
+    {
+        name: "kitty"
+    },
+    {
+        name: "Tom jack"
+    },
+    {
+        name: "VMatrix"
+    },
+    {
+        name: "aaaaaa"
+    },
+    {
+        name: "bbbbbb"
+    },
+    {
+        name: "cccccc"
+    },
+    {
+        name: "dddddd"
+    }
+])
+
+function handleClickSportingItem(item) {
+    sportingList.forEach(item => (item.active = false))
+    item.active = true
+}
+</script>
+
+<style lang="scss" scoped>
+$border-color: rgb(218, 218, 218);
+
+.profile-bar {
+    display: flex;
+    padding-left: 30px;
+    .header-wrapper {
+        width: 100px;
+        padding-top: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .icon-wrapper {
+            height: 100px;
+            border-radius: 99px;
+            background: conic-gradient(rgb(97, 152, 253), rgb(97, 152, 253) 30%, rgb(219, 219, 219) 30%);
+            padding: 5px;
+            .header {
+                width: 100%;
+                height: 100%;
+                border-radius: 99px;
+            }
+        }
+        .score {
+            font-size: 13px;
+            margin-top: 5px;
+            padding: 2px 10px;
+            color: white;
+            font-weight: bold;
+            background-color: rgba(0, 0, 0, 0.767);
+            border-radius: 8px;
+        }
+    }
+    .summoner-info {
+        margin-left: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: self-start;
+        justify-content: space-between;
+    }
+}
+
+.sporting-wrapper {
+    display: flex;
+    .sporting-list {
+        .sporting-item {
+            padding: 5px;
+            width: 200px;
+            border-radius: 5px;
+            background-color: white;
+
+
+            &:not(.active):hover {
+                background: rgb(241, 241, 241);
+            }
+
+            &.active {
+                position: relative;
+                border: 1px solid $border-color;
+                border-right: none;
+                border-radius: 5px 0 0 5px;
+                background: linear-gradient(to right, #1039d6, transparent);
+              .list-header {
+                width: 50px;
+                height: 50px;
+              }
+
+                &::after {
+                    position: absolute;
+                    content: "";
+                    background-color: white;
+                    width: 11px;
+                    border-left: none;
+                    border-right: none;
+                    top: -1px;
+                    bottom: -1px;
+                    right: -11px;
+                    border: 1px solid $border-color;
+                    border-left: none;
+                    border-right: none;
+                }
+            }
+
+            &:not(:last-child) {
+                margin-bottom: 10px;
+            }
+        }
+    }
+    .sporting-detail {
+        // z-index: 2;
+        margin-left: 10px;
+        flex: auto;
+        border: 1px solid $border-color;
+        border-radius: 0 5px 5px 0;
+    }
+}
+</style>
